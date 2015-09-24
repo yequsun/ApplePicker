c27e6aee9591ef04aa3f23150bf204b1cd0d1040
Yequ Sun <yequsun@gmail.com>
2015-09-24 16:55:37 -0400

    update

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Spawn : MonoBehaviour {
 	public GameObject basketPrefab;
 	public int numBaskets = 3;
 	public float basketBottomY = -4.5f;
 	public float basketSpacingY = 0f;
-
+	public Text scoreText;
+	public int score;
 	// Use this for initialization
 	void Start () {
+		score = 0;
+
 		for (int i=0; i<numBaskets; i++) {
 			GameObject tBasketGo = Instantiate (basketPrefab) as GameObject;
 			Vector3 pos = Vector3.zero;
@@ -20,6 +24,7 @@ public class Spawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		scoreText.text = "Score: " + score;
 	
 	}
 }
